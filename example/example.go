ffc318f67c2bf5cc9ea3b58975fc005f474284ae
Kamil Kisiel <kamil@kamilkisiel.net>
2012-05-30 23:42:25 -0700

    Made the example a little more like the Python client

--- example/example.go
+++ example/example.go
@@ -1,24 +1,41 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"github.com/kisielk/raven-go/raven"
+	"os"
+	"strings"
 )
 
 func main() {
-	dsn := flag.String("dsn", "", "Sentry dsn")
-	flag.Parse()
 
-	if *dsn == "" {
-		fmt.Printf("You need to use the --dsn flag to specify the Sentry server's dsn\n")
+	var dsn string
+	if len(os.Args) > 2 {
+		dsn = strings.Join(os.Args[2:], " ")
+	} else {
+		dsn = os.Getenv("SENTRY_DSN")
+	}
+
+	if dsn == "" {
+		fmt.Printf("Error: No configuration detected!\n")
+		fmt.Printf("You must either pass a DSN to the command, or set the SENTRY_DSN environment variable\n")
 		return
 	}
 
-	fmt.Printf("Connecting to dsn: %v\n", *dsn)
-	client, err := raven.NewRavenClient(*dsn)
+	fmt.Printf("Using DSN configuration:\n %v\n", dsn)
+	client, err := raven.NewRavenClient(dsn)
+
 	if err != nil {
-		fmt.Printf("Could not connect: %v", dsn)
+		fmt.Printf("could not connect: %v", dsn)
 	}
-	client.CaptureMessage("Hello world")
+
+	fmt.Printf("Sending a test message...\n")
+	id, err := client.CaptureMessage("This is a test message generated using ``goraven test``")
+
+	if err != nil {
+		fmt.Printf("failed: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Message captured, id: %v", id)
 }
